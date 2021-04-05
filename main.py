85b494935344c1f0e07fc6d352c4834be1f29507
Paweł Kopel <pawel.kopel2@gmail.com>
2021-04-05 15:54:40 +0200

    refactor code to work without lcd

--- main.py
+++ main.py
@@ -11,39 +11,51 @@ I2C_NUM_COLS = 16
 
 print("Zaczynam działanie")
 i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
-print(i2c.scan())
+i2c_scan = i2c.scan()
 
-lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+print_fun = print
 
+if len(i2c_scan) > 0 and i2c_scan[0] == I2C_ADDR:
+    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+    def print_lcd(msg: str):
+        lcd.clear()
+        lcd.putstr(msg)
+    print_fun = print_lcd
+        
 sensorPin = machine.Pin(28, machine.Pin.OUT, machine.Pin.PULL_DOWN)
 sensor = DHT11(sensorPin)
 
 button1 = machine.Pin(2, machine.Pin.IN, machine.Pin.PULL_DOWN)
 button2 = machine.Pin(3, machine.Pin.IN, machine.Pin.PULL_DOWN)
-button3 = machine.Pin(4, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-current = "celsius"
+def fahr_format(temp: float) -> str:
+    return "Temp: {} F".format(temp*1.8 + 32)
 
-lcd.putstr("Działa!")
-utime.sleep(2)
-lcd.clear()
+def celsius_format(temp: float) -> str:
+    return "Temp: {} C".format(temp)
 
-while True:
-    if button1.value():
-        current = "celsius"
-    elif button2.value():
-        current = "fahr"
-    elif button3.value():
-        current = "hum"
-        
-    lcd.clear()
-    
-    output = "Temp: {} C".format(sensor.temperature)
-    
-    if current == "fahr":
-        output = "Temp: {} F".format(sensor.temperature*1.8 + 32)
-    elif current == "hum":
-        output = "Hum: {} %".format(sensor.humidity)
-        
-    lcd.putstr(output)
+def hum_format(hum: float) -> str:
+    return "Hum: {} %".format(hum)
+
+def run_station():
+    current = "celsius"
+    print_fun("Działa!")
     utime.sleep(2)
+    #main loop
+    while True:
+        if button1.value():
+            current = "celsius"
+        elif button2.value():
+            current = "fahr"
+        
+        temp = sensor.temperature
+        hum = sensor.humidity
+        output_temp = fahr_format(temp) if current == "fahr" else celsius_format(temp)
+        output_hum = hum_format(hum)
+        
+        output = "{}\n{}".format(output_temp, output_hum)
+            
+        print_fun(output)
+        utime.sleep(2)
+
+run_station()
